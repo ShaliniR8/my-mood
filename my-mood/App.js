f80e14889aefcd0b710d978158356a4c116090c2
ShaliniR8 <1705820@kiit.ac.in>
2023-01-26 04:18:13 -0600

    navigation container set up

--- my-mood/App.js
+++ my-mood/App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
+    <NavigationContainer>
     <View className="flex-1 items-center justify-center bg-white">
-      <Text>Did something!</Text>
+      <Text>Inside Navigation container!</Text>
       <StatusBar style="auto" />
     </View>
+    </NavigationContainer>
   );
 }
 
