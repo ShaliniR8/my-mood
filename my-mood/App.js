b331147b496554fb4530f4636e41dbfff5d35a39
ShaliniR8 <1705820@kiit.ac.in>
2023-01-26 04:25:41 -0600

    Home Screen

--- my-mood/App.js
+++ my-mood/App.js
@@ -1,14 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Home!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Inside Navigation container!</Text>
-      <StatusBar style="auto" />
-    </View>
+      {/* different screens inside our app through nav container */}
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
